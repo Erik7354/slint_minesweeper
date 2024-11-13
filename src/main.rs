77796e7324c955e536d0521ba01732451f3d3ab4
Erik <31774332+Erik7354@users.noreply.github.com>
2024-11-13 10:08:47 +0100

    fix panic without query params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 mod minesweeper;
 mod ui;
 
@@ -9,26 +11,35 @@ pub fn main() {
     ui::run(ms).unwrap();
 }
 
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 #[cfg(target_arch = "wasm32")]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
 pub fn main() {
+    log!("Welcome to Minesweeper made with Rust+Slint and running on WASM!");
+
     let mut settings: minesweeper::Settings = minesweeper::BEGINNER_SETTINGS;
 
     let win = web_sys::window().unwrap();
-
     let s = win.location().search().unwrap();
 
-    let query: Vec<&str> = s.trim_start_matches("?").split("&").collect();
-    for entry in query {
-        let pair: Vec<&str> = entry.split("=").collect();
-        let key = pair[0];
-        let val = pair[1];
+    let query = s.trim_start_matches("?");
+    if query != "" {
+        for entry in query.split("&").collect::<Vec<_>>() {
+            let pair: Vec<&str> = entry.split("=").collect();
+            let key = pair[0];
+            let val = pair[1];
 
-        match key {
-            "width" => settings.dx = val.parse::<usize>().unwrap(),
-            "height" => settings.dy = val.parse::<usize>().unwrap(),
-            "mine_count" => settings.mine_count = val.parse::<usize>().unwrap(),
-            _ => ()
+            match key {
+                "width" => settings.dx = val.parse::<usize>().unwrap(),
+                "height" => settings.dy = val.parse::<usize>().unwrap(),
+                "mine_count" => settings.mine_count = val.parse::<usize>().unwrap(),
+                _ => ()
+            }
         }
     }
 
